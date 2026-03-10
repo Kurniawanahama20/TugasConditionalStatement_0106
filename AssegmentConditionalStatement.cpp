c39d15e492bb4eabb40d45cf381a4152a7b739b3
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-10 21:02:52 +0700

    commit1 "ProcedurForInputData"

---
+++ AssegmentConditionalStatement.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//procedur for input data
+void inputData(float &berat, float &tinggi) {
+    cout << "masukkan berat badan (kg): ";
+    cin >> berat;
+    
+}
